3e666eaa83214433fea00d8457610f7cec825944
yyyyyyxl <yyyxl7047@163.com>
2023-04-30 15:50:20 +0800

    初始化工程-init ddd project

---
+++ lottery-interfaces/src/main/java/com/yyyxl/lottery/LotteryApplication.java
@@ -0,0 +1,15 @@
+package com.yyyxl.lottery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yyyxl
+ * @description 启动入口
+ */
+@SpringBootApplication
+public class LotteryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LotteryApplication.class,args);
+    }
+}
---
+++ lottery-interfaces/src/test/java/com/yyyxl/lottery/test/LotteryTest.java
@@ -0,0 +1,9 @@
+package com.yyyxl.lottery.test;
+
+
+/**
+ * @author yyyxl
+ * @description 单元测试
+ */
+public class LotteryTest {
+}
