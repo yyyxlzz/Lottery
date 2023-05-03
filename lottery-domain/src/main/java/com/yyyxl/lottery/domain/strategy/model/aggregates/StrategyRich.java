4e60c35544f022919a4e556ea883cff06f8cce9c
yyyyyyxl <yyyxl7047@163.com>
2023-05-03 16:11:57 +0800

    第06节：板模式处理抽奖流程

--- lottery-domain/src/main/java/com/yyyxl/lottery/domain/strategy/model/aggregates/StrategyRich.java
+++ lottery-domain/src/main/java/com/yyyxl/lottery/domain/strategy/model/aggregates/StrategyRich.java
@@ -6,11 +6,6 @@ import com.yyyxl.lottery.infrastructure.po.StrategyDetail;
 
 import java.util.List;
 
-/**
- * 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
- * 公众号：bugstack虫洞栈
- * Create by 小傅哥(fustack)
- */
 public class StrategyRich {
 
     // 策略ID
